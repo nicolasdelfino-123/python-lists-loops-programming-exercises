d67c94ad014314eb99ffa7e1e7baceb4f240d164
Nicolas <nicolasdelfino585@gmail.com>
2025-04-21 11:41:57 -0300

    repaso ejercicio 1.5

--- exercises/01.5-Add-item-to-list/app.py
+++ exercises/01.5-Add-item-to-list/app.py
@@ -4,11 +4,9 @@ import random
 my_list = [4, 5, 734, 43, 45]
 
 # The magic goes below
-num_aleatorios = []
-for i in range(1,11):
-   num_aleatorios.append(random.randint(1,10))
-   my_list.append(num_aleatorios)
-    
 
+for x in range(0,10):
+    n = random.randint(0,20)
+    my_list.append(n)
 
-
+print(my_list)
